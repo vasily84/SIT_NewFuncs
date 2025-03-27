67cd50e986b3b8b7cd8dd48911ee032251e65dd5
vasily84 <35914841+vasily84@users.noreply.github.com>
2025-03-27 13:28:16 +0300

    1
    diffxy - функция SimInTech вычисления конечной разности для таблицы

---
+++ SIT_Source/source/NewFuncPLUGIN/Info.pas
@@ -0,0 +1,67 @@
+﻿
+{$IFDEF FPC}
+  {$MODE Delphi}
+{$ENDIF}
+
+unit Info;
+
+//**************************************************************************//
+// Данный исходный код является составной частью системы SimInTech          //
+// Авторские права на данный код принадлежат ООО 3В-Сервис                  //
+//       Авторы: Тимофеев К.А.,                                             //
+//**************************************************************************//
+
+interface
+
+uses
+  Classes,
+  DataTypes,
+  InterfaceUnit;
+
+// Инициализация библиотеки
+function  Init(): Boolean;
+// Процедура создания объекта
+function  CreateObject(Owner:Pointer;const Name: string):Pointer;
+// Уничтожение библиотеки
+procedure Release;
+
+// Главная информационая запись библиотеки
+// Она содержит ссылки на процедуры инициализации, завершения библиотеки
+// и функцию создания объектов
+const
+  DllInfo: TDllInfo =
+  (
+    Init:         Init;
+    Release:      Release;
+    CreateObject: CreateObject;
+  );
+
+
+implementation
+
+uses  uNewFunctions;
+
+function  Init(): Boolean;
+begin
+  // Если библиотека инициализирована правильно, то функция должна вернуть True
+  Result := True;
+  // Присваиваем папку с корневой директорией базы данных программы
+  DBRoot := DllInfo.Main^.DataBasePath^;
+  // Регистрируем функции для доступа к ним из проектов
+  DllInfo.Main^.RegisterFuncs(@NewFuncs, Length(NewFuncs));
+end;
+
+// Процедура создания объектов
+// Возвращает интерфейс объекту-плагину
+function  CreateObject(Owner:Pointer; const Name: String): Pointer;
+begin
+  Result := nil;
+end;
+
+procedure Release;
+begin
+
+end;
+
+
+end.
---
+++ SIT_Source/source/NewFuncPLUGIN/uNewFunctions.pas
@@ -0,0 +1,65 @@
+﻿unit uNewFunctions;
+
+{$IFDEF FPC}
+  {$MODE Delphi}
+{$ENDIF}
+
+interface
+
+uses DataTypes, FuncTypes, SysUtils;
+function diffxy_info(Sender: TOpContainer;Action: NativeInt;ArgIndex: NativeInt;IsAssign: boolean;var Params:TInfoParams):NativeInt;
+function diffxy_run(Sender: TCustomFunc):NativeInt;
+
+const
+
+ //  Функции для встроенного скриптового языка для работы
+ NewFuncs : array [0..0] of TFuncInfo = (
+   (Name:'diffxy';RunFunc:diffxy_run;     InfoFunc:diffxy_info)
+ );
+
+implementation
+
+function diffxy_info(Sender: TOpContainer;Action: NativeInt;ArgIndex: NativeInt;IsAssign: boolean;var Params:TInfoParams):NativeInt;
+begin
+ Result:=0;
+ case Action of
+   a_GetArgCount: Result:=2;
+   a_GetArgType:  case ArgIndex of
+                    0,1: Result:=dtDoubleArray;
+                  else
+                    Result:=dtInteger;
+                  end;
+   a_GetResType:  Result:=dtDoubleArray;
+ end;
+end;
+
+function diffxy_run(Sender: TCustomFunc):NativeInt;
+// построить конечную разность.
+// вход - x_arr = […];y_arr = […]; -массивы
+// выход y_dot_arr = func(x_arr, y_arr); - массив
+// длина выхода = [минимальная] длина входного массива -1.
+var
+  i,nCounts:Integer;
+  dx,dy,s:RealType;
+  argX, argY, resultArr:TExtArray;
+begin
+  Result:=0;
+  resultArr:=TExtArray(Sender.Data);
+  argX:=TExtArray(Sender.Args[0].Data);
+  argY:=TExtArray(Sender.Args[1].Data);
+
+  nCounts := argX.Count;
+  if nCounts> argY.Count then nCounts := argY.Count;
+
+  resultArr.Count:=nCounts-1;
+
+  for i:=0 to nCounts-2 do begin
+    dx:=argX[i+1]-argX[i];
+    dy:=argY[i+1]-argY[i];
+    s:=dy/dx;
+    resultArr.Arr[i]:=s;
+    end;
+
+end;
+
+end.
